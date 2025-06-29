e7fd17c8227a3cd93653f46d183735ff22349379
김민솔 <gwsak@dgu.ac.kr>
2025-06-29 20:19:27 +0900

    [Gold I] Title: 이항 계수 3, Time: 144 ms, Memory: 44988 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -2,21 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static final int DIV = 1000000007;
+    static final long DIV = 1000000007;
+    static long[] fac;
+    
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long N = Long.parseLong(st.nextToken());
-        long K = Long.parseLong(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         
-        System.out.println((fac(N)*pow((fac(K)*fac(N-K))%DIV, DIV-2)) % DIV);
+        fac = new long[N+1];
+        fac[0] = 1;
+        for(int i=1; i<=N; i++){
+            fac[i] = (fac[i-1]*i) % DIV;
+        }
+        
+        System.out.println((fac[N]*pow((fac[K]*fac[N-K])%DIV, DIV-2)) % DIV);
     }
-    
-    static long fac(long num){
-        if(num == 0) return 1;
-        return num*fac(num-1) % DIV;
-    }
-    
+
     static long pow(long a, long b){
         long result = 1;
         while(b>0){
