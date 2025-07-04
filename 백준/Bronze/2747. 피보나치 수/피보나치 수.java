69f278f0b72852aae519bc4b3b7a4e022228fc60
김민솔 <gwsak@dgu.ac.kr>
2025-07-04 23:04:19 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -2,18 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static int[] memo = new int[46];
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(fib(n));
-    }
-    
-    static int fib(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        if(memo[n] != 0) return memo[n];
-        memo[n] = fib(n-1)+fib(n-2);
-        return memo[n];
+        int[] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        System.out.println(dp[n]);
     }
 }
