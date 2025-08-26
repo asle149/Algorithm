4659acca81eeacffc2a4f27e85bd77f68298fd12
김민솔 <gwsak@dgu.ac.kr>
2025-08-27 01:14:40 +0900

    [Silver III] Title: 주유소, Time: 392 ms, Memory: 36556 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,48 +1,31 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
+
+        long[] dist = new long[N - 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] dist = new int[N-1];
-        int remain = 0;
-        for(int i=0; i<N-1; i++){
-            dist[i] = Integer.parseInt(st.nextToken());
-            remain += dist[i];
+        for (int i = 0; i < N - 1; i++) {
+            dist[i] = Long.parseLong(st.nextToken());
         }
-        
+
+        long[] price = new long[N];
         st = new StringTokenizer(br.readLine());
-        int[] cost = new int[N];
-        for(int i=0; i<N; i++){
-            cost[i] = Integer.parseInt(st.nextToken());
+        for (int i=0; i<N; i++) {
+            price[i] = Long.parseLong(st.nextToken());
         }
-        
-        int allCost = 0;
-        int cur = 0;
-        for(int i=0; i<N-1; i++){
-            boolean isMin = true;
-            for(int j=i; j<N-1; j++){
-                if(cost[i]>cost[j]) { isMin = false; continue; }
-            }
-            if(isMin){
-                allCost += remain*cost[i];
-                cur = remain;
-                remain = 0;
-                continue;
-            }
-            
-            if(cur<dist[i]){
-                allCost += dist[i]*cost[i];
-                cur += dist[i];
-                remain -= dist[i];
-            }
-            
-            cur -= dist[i];
+
+        long ans = 0;
+        long minPrice = price[0]; // 지금까지의 최저가
+
+        for (int i=0; i<N-1; i++) {
+            if (price[i] < minPrice) minPrice = price[i];
+            ans += minPrice * dist[i]; // 최저가로 다음 구간 이동 비용 누적
         }
-        
-        System.out.println(allCost);            
+
+        System.out.println(ans);
     }
 }
