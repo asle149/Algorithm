53ea7942671a2733eba8f3defbaa92360b4e1d91
김민솔 <gwsak@dgu.ac.kr>
2025-09-11 17:44:32 +0900

    [Silver II] Title: 예산, Time: 144 ms, Memory: 15228 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); 
+                                 
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int left = 0;
+        int right = -1;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            right = Math.max(right, arr[i]);
+        }
+                                 
+        int M = Integer.parseInt(br.readLine());
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long budget = 0;
+            for (int i = 0; i < N; i++) {
+                budget += Math.min(arr[i], mid);
+            }
+            if (budget <= M) {
+                left = mid + 1;
+            } else {
+                right = mid -1;
+            }
+        }
+        System.out.println(right);
+    }
+}
