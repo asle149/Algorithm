69fa59f0e7be9fb97878a2799b3eeb5fb2cb9a11
김민솔 <gwsak@dgu.ac.kr>
2026-01-20 15:54:58 +0900

    [level 2] Title: 전화번호 목록, Time: 367.68 ms, Memory: 98.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] pb) {
+        Arrays.sort(pb);
+        for(int i=0; i<pb.length-1; i++){
+            if(pb[i+1].startsWith(pb[i])) return false;
+            // for(int j=i+1; j<pb.length; j++){
+            //     if(pb[j].startsWith(pb[i])) return false;
+            // }
+        }
+        return true;
+    }
+}
