e2ebf6e659aebcd2fc026ca516541993eead09c1
김민솔 <gwsak@dgu.ac.kr>
2026-02-09 16:27:02 +0900

    [level 1] Title: 모의고사, Time: 1.42 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,55 +1,29 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    public boolean aa(int i, int ans){
-        if(i%5 == 1 && ans == 1) return true;
-        if(i%5 == 2 && ans == 2) return true;
-        if(i%5 == 3 && ans == 3) return true;
-        if(i%5 == 4 && ans == 4) return true;
-        if(i%5 == 0 && ans == 5) return true;
-        return false;
-    }
-    public boolean bb(int i, int ans){
-        if(i%2 == 1) {if(ans == 2) return true;}
-        else{
-            if((i/2)%4 == 1 && ans == 1) return true; 
-            if((i/2)%4 == 2 && ans == 3) return true; 
-            if((i/2)%4 == 3 && ans == 4) return true; 
-            if((i/2)%4 == 0 && ans == 5) return true; 
-        }
-        return false;
-    }
-    
-    public boolean cc(int i, int ans){
-        if(((i+1)/2)%5 == 1 && ans == 3) return true;
-        if(((i+1)/2)%5 == 2 && ans == 1) return true;
-        if(((i+1)/2)%5 == 3 && ans == 2) return true;
-        if(((i+1)/2)%5 == 4 && ans == 4) return true;
-        if(((i+1)/2)%5 == 0 && ans == 5) return true;
-        return false;
-    }
-    
-    public int[] solution(int[] answer) {
-        int corA = 0, corB = 0, corC = 0;
-        for(int i=1; i<=answer.length; i++){
-            if(aa(i, answer[i-1])) corA++;
-            if(bb(i, answer[i-1])) corB++;
-            if(cc(i, answer[i-1])) corC++;
-        }
-        
-        int max = 0;
-        max = Math.max(corA, Math.max(corB, corC));
-        
-        List<Integer> l = new ArrayList<>();
-        if(corA == max) l.add(1);
-        if(corB == max) l.add(2);
-        if(corC == max) l.add(3);
+    public static int[] solution(int[] answers) {
+        int[][] patterns = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
 
-        int[] result = new int[l.size()];
-        for (int i = 0; i < l.size(); i++) {
-            result[i] = l.get(i); 
+        int[] hit = new int[3];
+        for(int i = 0; i < hit.length; i++) {
+            for(int j = 0; j < answers.length; j++) {
+                if(patterns[i][j % patterns[i].length] == answers[j]) hit[i]++;
+            }
         }
-        return result;
+
+        int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < hit.length; i++)
+            if(max == hit[i]) list.add(i + 1);
+
+        int[] answer = new int[list.size()];
+        int cnt = 0;
+        for(int num : list)
+            answer[cnt++] = num;
+        return answer;
     }
 }
