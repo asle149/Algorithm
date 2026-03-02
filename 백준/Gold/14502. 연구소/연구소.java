9c3c420a1eaf86bedd242c91420f3fea711f9fbe
김민솔 <gwsak@dgu.ac.kr>
2026-03-03 00:17:50 +0900

    [Gold IV] Title: 연구소, Time: 324 ms, Memory: 125684 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int N, M, wall, ans;
+    static int[][] map;
+    static List<int[]> virus = new ArrayList<>();
+    static List<int[]> empty = new ArrayList<>();
+    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+    
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        wall = 0; ans = 0;
+        map = new int[N][M];
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());  
+                if(map[i][j] == 2) virus.add(new int[]{i, j});
+                else if(map[i][j] == 0) empty.add(new int[]{i, j});
+            }
+        }
+        dfs(map, 0, 0);
+        System.out.println(ans);
+    }
+    
+    static void dfs(int[][] map, int start, int idx){
+        if(idx == 3){
+            int size = bfs(map);
+            ans = Math.max(size, ans);
+            return;
+        }
+        for(int i=start; i<empty.size(); i++){
+            int[] cur = empty.get(i);
+            int x = cur[0], y = cur[1];
+            map[x][y] = 1;
+            dfs(map, i+1, idx+1);
+            map[x][y] = 0;
+        }
+    }
+    
+    static int bfs(int[][] map){
+        int infected = 0;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[N][M];
+        for(int i=0; i<virus.size(); i++){
+            q.offer(virus.get(i));
+        }
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            visited[x][y] = true;
+            for(int k=0; k<4; k++){
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                if(nx>=0 && ny>=0 && nx<N && ny<M && !visited[nx][ny] && map[nx][ny] == 0){
+                    visited[nx][ny] = true;
+                    q.offer(new int[]{nx, ny});
+                    infected++;
+                }
+            }
+        }
+        return empty.size() - 3 - infected; 
+    }
+}
