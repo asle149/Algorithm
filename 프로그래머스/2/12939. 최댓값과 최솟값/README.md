d1e77f924de128f28cd89abd75d5818832515eb5
김민솔 <asle14910@gmail.com>
2026-06-30 23:34:18 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 3.31 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 83.6 MB, 시간: 0.34 ms
+메모리: 88.3 MB, 시간: 3.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 17일 18:00:40
+2026년 06월 30일 23:23:47
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,19 +2,14 @@ import java.util.*;
 
 class Solution {
     public String solution(String s)  {
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(s);
-        
-        int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         while(st.hasMoreTokens()){
-            int n = Integer.parseInt(st.nextToken());
-            min = Math.min(min, n);
-            max = Math.max(max, n);
+            int i = Integer.parseInt(st.nextToken());
+            max = Math.max(i, max);
+            min = Math.min(i, min);
         }
-        
-        sb.append(min).append(" ").append(max);
-        String answer = sb.toString();
-        return answer;
+        return min + " " + max;
     }
 }
