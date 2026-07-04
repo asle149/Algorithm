55f69ae8bd978a3f69b3adddbfda12b306d1047e
김민솔 <asle14910@gmail.com>
2026-07-04 21:38:13 +0900

    [level 2] Title: 짝지어 제거하기, Time: 54.12 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> st = new Stack<>();
+        st.push(s.charAt(0));
+        for(int i=1; i<s.length(); i++){
+            if(!st.isEmpty() && (st.peek() == s.charAt(i))) st.pop();
+            else st.push(s.charAt(i));
+        }
+        
+        if(st.isEmpty()) return 1;
+        else return 0;
+    }
+}
