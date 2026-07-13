23cc8374aaa9f67dbfe44814bdd6cf26973aada0
김민솔 <asle14910@gmail.com>
2026-07-13 11:49:14 +0900

    [level 2] Title: 롤케이크 자르기, Time: 152.12 ms, Memory: 236 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -10,8 +10,8 @@ class Solution {
         for(int i=0; i<top.length; i++) 
             map1.put(top[i], map1.getOrDefault(top[i], 0)+1);
         
-        for(int i=top.length-1; i>0; i--){
-            map2.put(top[i], map1.getOrDefault(top[i], 0)+1);
+        for(int i=0; i<top.length-1; i++){
+            map2.put(top[i], map2.getOrDefault(top[i], 0)+1);
             if(map1.get(top[i]) == 1) map1.remove(top[i]);
             else map1.put(top[i], map1.get(top[i])-1);
             if(map1.size() == map2.size()) ans++;
