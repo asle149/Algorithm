cadebe9c8aae93b2f28f53c0b3272e1533f94f88
김민솔 <asle14910@gmail.com>
2026-07-25 16:31:54 +0900

    [level 2] Title: 택배상자, Time: 67.46 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,20 +1,6 @@
-import java.io.*;
-import java.util.*;
-
 class Solution {
     public int solution(int[] order) {
-        Stack<Integer> sub = new Stack<>();
-        int p = 0;
-        
-        for(int i=1; i<=order.length; i++){
-            if(i == order[p]) p++;
-            else sub.push(i);
-            while(!sub.isEmpty() && sub.peek() == order[p]){
-                sub.pop();
-                p++;
-            }
-        }
-        
-        return p;
+        int answer = 0;
+        return answer;
     }
 }
