90bd50a09ae5909f5a2f285b8cb7036a9764b915
김민솔 <asle14910@gmail.com>
2026-07-27 15:48:40 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.22 ms, Memory: 57.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static int DIV = 1_000_000_007;
+    
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1; dp[2] = 2; 
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1]+dp[i-2])%DIV;
+        }
+        
+        return dp[n];        
+    }
+}
