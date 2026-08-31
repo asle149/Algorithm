e24baaa18d08f97c2163b4e8a8b2fc16ddcccf49
김민솔 <asle14910@gmail.com>
2026-08-31 12:44:43 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,48 +1,35 @@
+import java.io.*;
 import java.util.*;
 
-class Solution {
-    static class Node {
-        String w;
-        int d;
-
-        Node(String w, int d) {
-            this.w = w;
-            this.d = d;
-        }
-    }
-
+class Solution{
+    static boolean[] visited;
+    static int ans = Integer.MAX_VALUE;
+    static String[] words;
+    static String target;
+    
     public int solution(String begin, String target, String[] words) {
-        boolean check = false;
-        for (String s : words) {
-            if (s.equals(target)) check = true;
-        }
-        if (!check) return 0;
-
-        Queue<Node> q = new LinkedList<>();
-        boolean[] v = new boolean[words.length];
-
-        q.offer(new Node(begin, 0));
-
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-
-            if (cur.w.equals(target)) return cur.d;
-
-            for (int i = 0; i < words.length; i++) {
-                if (!v[i] && diff(cur.w, words[i])) {
-                    v[i] = true;
-                    q.offer(new Node(words[i], cur.d + 1));
-                }
+        visited = new boolean[words.length];
+        Solution.target = target;
+        Solution.words = words;
+        dfs(begin, 0);
+        if(ans == Integer.MAX_VALUE) return 0;
+        return ans; 
+    }
+    
+    public void dfs(String word, int cnt){
+        if(word.equals(target)) {ans = Math.min(cnt, ans); return;}
+        for(int i=0; i<words.length; i++){
+            int diff = 0;
+            for(int j=0; j<word.length(); j++){
+                if(word.charAt(j) != words[i].charAt(j)) diff++;
+            }
+            
+            if(diff == 1 && !visited[i]){
+                visited[i] = true;
+                dfs(words[i], cnt+1);
+                visited[i] = false;
             }
         }
-        return 0;
-    }
-
-    static boolean diff(String a, String b) {
-        int c = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) c++;
-        }
-        return c == 1;
+        return;
     }
 }
