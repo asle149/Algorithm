e530f8f03e284b2b4e0b7bcb867e4c8d933df21f
김민솔 <asle14910@gmail.com>
2026-08-31 12:11:25 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
-    int n;
-    int[][] computers;
-    boolean[] visited;
+    static boolean[] visited, check;
+    static int[][] coms;
     
-    public int solution(int n, int[][] computers) {
-        this.n = n;
-        this.computers = computers;
-        this.visited = new boolean[n];
+    public int solution(int n, int[][] coms) {
         int ans = 0;
-        
+        visited = new boolean[n];
+        Solution.coms = coms;
         for(int i=0; i<n; i++){
-            if(!visited[i]){
+            if(!visited[i]) {
                 dfs(i);
                 ans++;
             }
         }
-        
         return ans;
     }
     
     public void dfs(int num){
         visited[num] = true;
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i] && computers[num][i] == 1) dfs(i);
+        for(int i=0; i<coms[num].length; i++){
+            if(coms[num][i] == 1 && !visited[i]) dfs(i);
         }
+        return;
     }
 }
