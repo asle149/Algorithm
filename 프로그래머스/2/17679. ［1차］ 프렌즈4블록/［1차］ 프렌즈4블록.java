5eb07c49378d0d8cc53a4de346504c53df90e37b
김민솔 <asle14910@gmail.com>
2026-09-18 15:03:17 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 5.68 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -2,62 +2,65 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    static int m, n;
+    static boolean isFinish = true;
+    
     public int solution(int m, int n, String[] board) {
-        char[][] b = new char[board.length][board[0].length()];
-        for(int i=0; i<board.length; i++) b[i] = board[i].toCharArray();
-
-        while(true){
-            char[][] d = drop(b);
-            if(!check(d)) break;
-            
-            for(int j=0; j<d[0].length; j++){
-                Stack<Character> st = new Stack<>();
-                for(int i=0; i<d.length; i++){
-                    if(d[i][j] != '1' && d[i][j] != '0') st.push(d[i][j]);
-                }
-                for(int i=d.length-1; i>=0; i--){
-                    if(st.isEmpty()) b[i][j] = '0';
-                    else { b[i][j] = st.pop(); }
-                }
-            }   
+        Solution.m = m;
+        Solution.n = n;
+        
+        char[][] arr = new char[m][n];
+        for(int i=0; i<m; i++){
+            arr[i] = board[i].toCharArray();
+        }
+        
+        while(isFinish){
+            arr = next(arr);
         }
         
         int ans = 0;
-        for(int i=0; i<b.length; i++){
-            for(int j=0;j<b[0].length; j++){
-                if(b[i][j] == '0') ans++;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j] == '\u0000') ans++;
             }
         }
+        
         return ans;
     }
     
-    public static char[][] drop(char[][] b){
-        char[][] arr = new char[b.length][b[0].length];
-        for(int i=0; i<arr.length; i++) arr[i] = b[i].clone();
-        
-        for(int i=0; i<b.length-1; i++){
-            for(int j=0; j<b[0].length-1; j++){
-                char c = b[i][j];
-                char r = b[i][j+1];
-                char d = b[i+1][j];
-                char dia = b[i+1][j+1];
-                if(c == '0') continue;
-                if(c == r && c == d && c == dia){
-                    arr[i][j] = '1'; arr[i][j+1] = '1';
-                    arr[i+1][j] = '1'; arr[i+1][j+1] = '1';
+    public char[][] next(char[][] arr){
+        boolean[][] check = new boolean[m][n];
+        isFinish = false;
+        for(int i=0; i<m-1; i++){
+            for(int j=0; j<n-1; j++){
+                if(arr[i][j] == '\u0000') continue;
+                char c = arr[i][j];
+                if(c == arr[i][j+1] && c == arr[i+1][j] && c == arr[i+1][j+1]){
+                    check[i][j] = true;
+                    check[i+1][j] = true;
+                    check[i][j+1] = true;
+                    check[i+1][j+1] = true;
+                    isFinish = true;
                 }
             }
         }
         
-        return arr;
+        char[][] next = drop(arr, check);
+        return next;
     }
     
-    public static boolean check(char[][] arr){
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
-                if(arr[i][j] == '1') return true;
+    public char[][] drop(char[][] arr, boolean[][] check){
+        char[][] drop = new char[m][n];
+        
+        for(int i=0; i<n; i++){
+            int j = m-1, k = m-1;
+            while(j>=0){
+                if(check[j][i]) {j--; continue;}
+                drop[k][i] = arr[j][i];
+                k--; j--;
             }
         }
-        return false;
+        
+        return drop;
     }
 }
